c27b7304c779569dccd294a17ab042e9d8d5fa20
Anderson Brasil <andersonbrasil72@gmail.com>
2023-02-07 22:16:50 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/// <reference types="nativewind/types" />
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-blue-700">
+      <Text className='text-white'>Open Lunch Time!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
